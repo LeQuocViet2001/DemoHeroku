4498e3146e0d6409e49d461580bf945961242722
LeQuocViet2001 <vietdat2012@gmail.com>
2022-11-13 09:31:07 +0700

    11/1323

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Imagetest;
 import com.example.demo.entity.Item;
-import com.example.demo.python.JythonCaller;
 import jep.JepException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,27 +32,27 @@ public class HelloController {
     }
 
 
-    @GetMapping("/list")
-    public ResponseEntity<List<String>> getListCustomer() {
-
-        List<String> A = null;
-        JythonCaller app = new JythonCaller();
-        try {
-            A = app.searchImage("323");
-        } catch (JepException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            List<String> itemList = A;
-            if (itemList.size() == 0) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-            return new ResponseEntity<>(itemList, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
+//    @GetMapping("/list")
+//    public ResponseEntity<List<String>> getListCustomer() {
+//
+//        List<String> A = null;
+//        JythonCaller app = new JythonCaller();
+//        try {
+//            A = app.searchImage("323");
+//        } catch (JepException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            List<String> itemList = A;
+//            if (itemList.size() == 0) {
+//                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//            }
+//            return new ResponseEntity<>(itemList, HttpStatus.OK);
+//        } catch (Exception e) {
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+//    }
 
 
     @GetMapping("/test")
@@ -78,93 +77,93 @@ public class HelloController {
 
 
 
-    @PostMapping("/test")
-    public ResponseEntity<List<Imagetest>> post3Layer(@RequestBody Imagetest imagetest) {
-        try {
+//    @PostMapping("/test")
+//    public ResponseEntity<List<Imagetest>> post3Layer(@RequestBody Imagetest imagetest) {
+//        try {
+//
+//
+//            List<String> A = new ArrayList<>();
+//            JythonCaller app = new JythonCaller();
+//            try {
+//                A = app.searchImage( imagetest.getNamefoto());
+//                System.out.println(  A);
+//            } catch (JepException e) {
+//                e.printStackTrace();
+//            }
+//
+//
+//            List<Imagetest> result = new ArrayList<>();
+//            List<String> itemList = A;
+//            if (itemList.size() == 0) {
+//                System.out.println("Khong co anh nao");
+//                return new ResponseEntity<>(  HttpStatus.NO_CONTENT);
+//            }
+//            else{
+////                String pathToImage = "C:\\Users\\ASUS\\Desktop\\Test\\test1\\src\\main\\resources\\static\\FootwearImg\\";
+//                String pathToImage = "src/main/resources/static/FootwearImg/";
+//                for ( String nameImage : itemList
+//                     ) {
+////                    System.out.println(nameImage);
+//                    byte[] bytes = Files.readAllBytes(Paths.get(pathToImage + nameImage));
+//                    String base64EncodedImageBytes = Base64.getEncoder().encodeToString(bytes);
+////                    System.out.println(base64EncodedImageBytes);
+////                    System.out.println(base64EncodedImageBytes);
+//                    Imagetest tmp = new Imagetest(nameImage, base64EncodedImageBytes, "png");
+//                    result.add(tmp);
+//                }
+//
+//            }
+////            System.out.println(result);
+//            return new ResponseEntity<>( result ,HttpStatus.OK);
+//
+//        } catch (Exception e) {
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+//    }
 
 
-            List<String> A = new ArrayList<>();
-            JythonCaller app = new JythonCaller();
-            try {
-                A = app.searchImage( imagetest.getNamefoto());
-                System.out.println(  A);
-            } catch (JepException e) {
-                e.printStackTrace();
-            }
 
-
-            List<Imagetest> result = new ArrayList<>();
-            List<String> itemList = A;
-            if (itemList.size() == 0) {
-                System.out.println("Khong co anh nao");
-                return new ResponseEntity<>(  HttpStatus.NO_CONTENT);
-            }
-            else{
-//                String pathToImage = "C:\\Users\\ASUS\\Desktop\\Test\\test1\\src\\main\\resources\\static\\FootwearImg\\";
-                String pathToImage = "src/main/resources/static/FootwearImg/";
-                for ( String nameImage : itemList
-                     ) {
-//                    System.out.println(nameImage);
-                    byte[] bytes = Files.readAllBytes(Paths.get(pathToImage + nameImage));
-                    String base64EncodedImageBytes = Base64.getEncoder().encodeToString(bytes);
-//                    System.out.println(base64EncodedImageBytes);
-//                    System.out.println(base64EncodedImageBytes);
-                    Imagetest tmp = new Imagetest(nameImage, base64EncodedImageBytes, "png");
-                    result.add(tmp);
-                }
-
-            }
-//            System.out.println(result);
-            return new ResponseEntity<>( result ,HttpStatus.OK);
-
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
-
-
-
-    @PostMapping("/test1")
-    public ResponseEntity<List<Imagetest>> postResNet50(@RequestBody Imagetest imagetest) {
-        try {
-
-
-            List<String> A = new ArrayList<>();
-            JythonCaller app = new JythonCaller();
-            try {
-                A = app.searchImageNesNet50( imagetest.getNamefoto());
-                System.out.println(  A );
-            } catch (JepException e) {
-                e.printStackTrace();
-            }
-
-
-            List<Imagetest> result = new ArrayList<>();
-            List<String> itemList = A;
-            if (itemList.size() == 0) {
-                System.out.println("Khong co anh nao");
-                return new ResponseEntity<>(  HttpStatus.NO_CONTENT);
-            }
-            else{
-//                String pathToImage = "C:\\Users\\ASUS\\Desktop\\Test\\test1\\src\\main\\resources\\static\\FootwearImg\\";
-                String pathToImage = "src/main/resources/static/FootwearImg/";
-                for ( String nameImage : itemList
-                     ) {
-//                    System.out.println(nameImage);
-                    byte[] bytes = Files.readAllBytes(Paths.get(pathToImage + nameImage));
-                    String base64EncodedImageBytes = Base64.getEncoder().encodeToString(bytes);
-
-                    Imagetest tmp = new Imagetest(nameImage, base64EncodedImageBytes, "png");
-                    result.add(tmp);
-                }
-
-            }
-//            System.out.println(result);
-            return new ResponseEntity<>( result ,HttpStatus.OK);
-
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
+//    @PostMapping("/test1")
+//    public ResponseEntity<List<Imagetest>> postResNet50(@RequestBody Imagetest imagetest) {
+//        try {
+//
+//
+//            List<String> A = new ArrayList<>();
+//            JythonCaller app = new JythonCaller();
+//            try {
+//                A = app.searchImageNesNet50( imagetest.getNamefoto());
+//                System.out.println(  A );
+//            } catch (JepException e) {
+//                e.printStackTrace();
+//            }
+//
+//
+//            List<Imagetest> result = new ArrayList<>();
+//            List<String> itemList = A;
+//            if (itemList.size() == 0) {
+//                System.out.println("Khong co anh nao");
+//                return new ResponseEntity<>(  HttpStatus.NO_CONTENT);
+//            }
+//            else{
+////                String pathToImage = "C:\\Users\\ASUS\\Desktop\\Test\\test1\\src\\main\\resources\\static\\FootwearImg\\";
+//                String pathToImage = "src/main/resources/static/FootwearImg/";
+//                for ( String nameImage : itemList
+//                     ) {
+////                    System.out.println(nameImage);
+//                    byte[] bytes = Files.readAllBytes(Paths.get(pathToImage + nameImage));
+//                    String base64EncodedImageBytes = Base64.getEncoder().encodeToString(bytes);
+//
+//                    Imagetest tmp = new Imagetest(nameImage, base64EncodedImageBytes, "png");
+//                    result.add(tmp);
+//                }
+//
+//            }
+////            System.out.println(result);
+//            return new ResponseEntity<>( result ,HttpStatus.OK);
+//
+//        } catch (Exception e) {
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+//    }
 
 }
